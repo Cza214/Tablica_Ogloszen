8742b3b82006627fc09dde7286b38566b734b18e
Cza214 <cza2-14@o2.pl>
2017-12-14 20:39:41 +0100

    Advert edit and delete functions for User.

--- Advert_Project/src/AdvertBundle/Controller/DefaultController.php
+++ Advert_Project/src/AdvertBundle/Controller/DefaultController.php
@@ -2,12 +2,14 @@
 
 namespace AdvertBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
+
     /**
      * @Route("/")
      */
@@ -15,4 +17,24 @@ class DefaultController extends Controller
     {
         return $this->render('base.html.twig');
     }
+
+    /**
+     * Finds adverts by user and displays
+     *
+     * @Route("/user")
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function showByUserAction(){
+
+        $em = $this->getDoctrine()->getManager();
+
+        $adverts = $em->getRepository('AdvertBundle:Advert')->findBy([
+            'user' => $this->getUser()
+        ]);
+
+        return $this->render('advert/advert_content.html.twig', array(
+            'adverts' => $adverts,
+        ));
+    }
+
 }
